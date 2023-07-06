d15464add71e46d3405ce647191c3ac8a714ce28
ae0n1an <max.verhoef0@gmail.com>
2023-07-06 12:27:12 +1000

    added create game page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Game } from "./classes/game";
 import { Routes, Route } from 'react-router-dom';
 import DisplayGame from './DisplayGame';
+import CreateGame from './CreateGame';
 import Home from './Home';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="app">
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/createGame' element={<CreateGame/> }> </Route>
         <Route path='/game' element={<DisplayGame game = {new Game()}/>}> </Route>
       </Routes>
     </div>
--- src/Home.tsx
+++ src/Home.tsx
@@ -8,11 +8,26 @@ function Home() {
 
   return (
     <div className="Home">
-        <div className="mdl-textfield mdl-js-textfield">
-            <input className="mdl-textfield__input" type="text" placeholder= "Enter Name..." id="titleInput"></input>
-        </div>
-        <br></br>
-        <Link to="/game">Start Game</Link>
+          <h6>Nickname:</h6>
+          <div className="mdl-textfield mdl-js-textfield">
+              <input className="mdl-textfield__input" type="text" placeholder= "Enter Nickname..." id="titleInput"></input>
+          </div>
+          <br></br>
+          <h6>Avatar:</h6>
+          <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label has-placeholder">
+              <select className="mdl-textfield__input">
+                  <option className="" value="">Select Avatar</option>
+                  <option className="" value="Green">Green</option>
+                  <option className="" value="Red">Red</option>
+                  <option className="" value="Blue">Blue</option>
+              </select>
+          </div>
+          <br></br>
+          <Link to="/createGame">Create Game</Link>
+          <br></br>
+          or
+          <br></br>
+          <Link to="/game">Join Game</Link>
     </div>
   );
 }
